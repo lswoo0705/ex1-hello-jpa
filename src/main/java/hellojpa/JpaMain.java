184de713a0ec4d347607f565337a35449dd4a1fc
sangwoo lee <lswoo0705@gmail.com>
2023-11-30 16:56:32 +0900

    Member Application

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,59 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 멤버 등록
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("helloB");
+//            em.persist(member);
+
+            // 멤버 찾기
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+
+            // 멤버 삭제
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember);
+
+            // 멤버 수정
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("helloJPA");
+            // 저장은 따로 안해도 수정은 성공한다
+//            em.persist(findMember);
+
+            // 리스트로 나타내기
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit(); // 적용하기
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
