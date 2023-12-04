0ee9bd33c564a9446930e9eea22b83f708622acd
sangwoo lee <lswoo0705@gmail.com>
2023-12-04 18:37:04 +0900

    Field and column mapping

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -83,12 +83,19 @@ public class JpaMain {
 //            em.flush(); // commit 전에 미리 db에 반영하고싶을 때 수동 플러시
 
             // 준영속
-            Member member = em.find(Member.class, 150L); // 찾아올 때 영속상태
-            member.setName("AAAAA");
-            // JPA에서 관리하지 않는 준영속 상태가 됨 -> name 업데이트(commit)가 되지 않는다.
-            em.detach(member); // 특정 Entity만
-            em.clear(); // em안에 영속성 컨텍스트 전부 지움
-            em.close(); // 영속성 컨텍스트를 종료
+//            Member member = em.find(Member.class, 150L); // 찾아올 때 영속상태
+//            member.setName("AAAAA");
+//            // JPA에서 관리하지 않는 준영속 상태가 됨 -> name 업데이트(commit)가 되지 않는다.
+//            em.detach(member); // 특정 Entity만
+//            em.clear(); // em안에 영속성 컨텍스트 전부 지움
+//            em.close(); // 영속성 컨텍스트를 종료
+
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
+
+            em.persist(member);
 
             System.out.println("=== COMMIT ===");
             tx.commit(); // 변경 내용을 db에 반영(플러시) -> 이 때 쿼리가 날아감
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,22 +1,33 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
     @Id
     private Long id;
-    @Column(unique = true, length = 10) // DDL 생성 기능
-    private String name;
+//    @Column(unique = true, length = 10) // DDL 생성 기능
+    @Column(name = "name", nullable = false)
+    private String username;
+    private int age;
+    @Enumerated(EnumType.STRING) // ORDINARY 사용하면 혼동 위험이 있으니 사용하지 말것
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date LastModifiedDate;
+    private LocalDate testLocalDate; // 년, 월, 일
+    private LocalDateTime testLocalDateTime; // 년, 월, 일, 시각
+    @Lob // 매핑하는 필드 타입이 문자면 CLOB 매핑, 나머지는 BLOB 매핑
+    private String description;
+    @Transient // DB에 관계없이 메모리에만 사용하고 싶을 때
+    private int temp;
 
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public Long getId() {
@@ -27,11 +38,59 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return LastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        LastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
     }
 }
