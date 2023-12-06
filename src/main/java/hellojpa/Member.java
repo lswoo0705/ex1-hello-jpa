cce349a18d67e974946c09f65c1d73b3e5e748e4
sangwoo lee <lswoo0705@gmail.com>
2023-12-06 14:42:46 +0900

    ManyToMany mapping

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,6 +2,9 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Member {
     @Id
@@ -16,6 +19,8 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
